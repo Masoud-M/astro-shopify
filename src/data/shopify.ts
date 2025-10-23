e1cd220bab4002b6feb317d36b6c0e19b15c601b
Masoud-M <48066903+Masoud-M@users.noreply.github.com>
2025-10-23 23:32:12 +0330

    chore: remove unnecessary codes and components

---
+++ src/data/shopify.ts
@@ -0,0 +1,6 @@
+export const PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN =
+  "41e621b45c8ecca32875390bef4d759b";
+export const PUBLIC_SHOPIFY_STORE_DOMAIN =
+  "https://astro-444365.myshopify.com/";
+export const PUBLIC_SHOPIFY_STORE_COUNTRY = "US";
+export const PUBLIC_SHOPIFY_STORE_LANGUAGE = "en";
