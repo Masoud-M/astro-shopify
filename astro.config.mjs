2c3d0e534ea1b352b4812e7ae3c71f47bf8b6d81
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2025-10-29 19:09:13 +0100

    Stable: shop sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,10 @@
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
 import netlify from "@astrojs/netlify";
 
 export default defineConfig({
-  output: 'static',
-
-  // update me!
-  site: "https://www.yourwebsite.com",
-
-  integrations: [
-    icon(),
-    sitemap({
-      filter: (page) => !page.includes("/admin"),
-      changefreq: "weekly",
-      priority: 0.7,
-    }),
-  ],
-
-  adapter: netlify(),
+	site: "https://www.yourwebsite.com",
+	integrations: [icon()],
+	adapter: netlify(),
 });
