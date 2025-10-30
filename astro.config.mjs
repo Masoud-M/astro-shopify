05586b43f096b443608a8b8a7fcc3c9122ac72de
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2025-10-30 17:13:15 +0100

    Refactor dynamic sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-
+import sitemap from "@astrojs/sitemap";
 import netlify from "@astrojs/netlify";
 
+const SITE_URL = "https://www.yourwebsite.com"; // REPLACE WITH YOUR SITE URL
+const SHOP_SLUG = "shop"; // REPLACE WITH YOUR SHOP SLUG
+
 export default defineConfig({
-	site: "https://www.yourwebsite.com",
-	integrations: [icon()],
+	site: SITE_URL,
+	integrations: [
+		icon(),
+		sitemap({
+			filter: (page) => !page.includes(`/${SHOP_SLUG}/`) || page === `${SITE_URL}/${SHOP_SLUG}/`,
+			customSitemaps: [`${SITE_URL}/${SHOP_SLUG}-sitemap.xml`],
+		}),
+	],
 	adapter: netlify(),
 });
